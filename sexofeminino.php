be0210bd18de1adeb3a270c558b6538b360d04fb
Niraldo Barbosa <->
2024-05-21 23:50:10 -0300

    tarefas php

--- sexofeminino.php
+++ sexofeminino.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Avaliação de Candidatura</title>
 </head>
-<body>
+<body> 
     <h1>Avaliação de Candidatura</h1>
 
     <?php
